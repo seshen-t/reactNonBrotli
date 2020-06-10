c6a7956ecda1d7f62342e4a511b59d5a223c4e0c
seshen-t <65315551+seshen-t@users.noreply.github.com>
2020-06-10 19:07:55 -0400

    Update Home.js

--- app/src/Home.js
+++ app/src/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my test site! This is confusing
+          Welcome to my test site! More confusion
         </p>
         <a
           className="App-link"
